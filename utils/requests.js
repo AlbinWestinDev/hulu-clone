c6a3f1ad5303bc5dbfb43dbe4f37162f2f9d4624
Albin Westin <albin.westin@xlent.se>
2022-01-24 15:44:53 +0100

    initial commit

---
+++ utils/requests.js
@@ -0,0 +1,48 @@
+const API_KEY = "8c8f51c5663318b1c2d0a837d92a68da";
+
+export default {
+  fetchTrending: {
+    title: "Trending",
+    url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchTopRated: {
+    title: "Top Rated",
+    url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchActionMovies: {
+    title: "Action",
+    url: `/discover/movie?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchComedyMovies: {
+    title: "Comedy",
+    url: `/discover/movie?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchHorrorMovies: {
+    title: "Horror",
+    url: `/discover/movie?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchRomanceMovies: {
+    title: "Romance",
+    url: `/discover/movie?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchMystery: {
+    title: "Mystery",
+    url: `/discover/movie?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchSciFi: {
+    title: "Sci-Fi",
+    url: `/discover/movie?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchWestern: {
+    title: "Western",
+    url: `/discover/movie?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchAnimation: {
+    title: "Animation",
+    url: `/discover/movie?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchTv: {
+    title: "Tv Movie",
+    url: `/discover/movie?api_key=${API_KEY}&language=en-US`,
+  },
+};
